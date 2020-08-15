e34c82a4614acf5dae345a77d128a483c0ef2988
Ge Yang <yangge1987@gmail.com>
2020-08-15 14:48:23 -0700

    add documentation string to pypi

--- setup.py
+++ setup.py
@@ -4,13 +4,17 @@ from setuptools import setup, find_packages
 with open(path.join(path.abspath(path.dirname(__file__)), 'VERSION'), encoding='utf-8') as f:
     version = f.read()
 
+with open(path.join(path.abspath(path.dirname(__file__)), 'README'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(name='gym-dmc',
       packages=find_packages(),
       install_requires=[
           "gym",
           "numpy",
       ],
-      description='gym-dmc',
+      description='gym-dmc is a gym wrapper arround DeepMind Controlsuite domains.',
+      long_description=long_description,
       author='Ge Yang<ge.ike.yang@gmail.com>',
       url='https://github.com/geyang/gym-dmc',
       author_email='ge.ike.yang@gmail.com',
