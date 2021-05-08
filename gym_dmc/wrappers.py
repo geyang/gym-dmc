7d9b7eacfa2896cd16f7c912cc2890b3c131b9f2
Ge Yang <yangge1987@gmail.com>
2021-05-07 22:02:10 -0400

    fix critical error of observation_space being flattened

--- gym_dmc/wrappers.py
+++ gym_dmc/wrappers.py
@@ -8,7 +8,7 @@ class ObservationByKey(ObservationWrapper):
     def __init__(self, env, obs_key):
         super().__init__(env)
         self.obs_key = obs_key
-        self.observation_space = spaces.flatten_space(env.observation_space[obs_key])
+        self.observation_space = env.observation_space[obs_key]
 
     def observation(self, observation):
         return observation[self.obs_key]
